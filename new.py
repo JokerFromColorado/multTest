452b0e63cc5e11e56ba6408ea50f361407c6c8f7
Mikhail <Mikhail.Kapusta@jetbrains.com>
2014-03-24 15:56:20 +0400

    #gh-38 Fixed Critical tag hhh

--- new.py
+++ new.py
@@ -5,8 +5,122 @@ sdfwewe
 ewwe
 
 wewe
-wertytryyrt564564564
-645456546456erteretrert
-ert
-etr
-etr
+wertytryyrt564564564ttweadasdsaasdttweadasdsaasdttweadasdsaasd
+645456546456erteretrertttweadasdsaasdttweadasdsaasdttweadasdsaasd
+erttwtwetwettweadasdsaasdttweadasdsaasdttweadasdsaasdttweadasdsaasd
+ttwetwettwettweadasdsaasdttweadasdsaasdttweadasdsaasdttweadasdsaasd
+ttwettweadasdsaasdttweadasdsaasdttweadasdsaasdttweadasdsaasdttweadasdsaasd
+twettweadasdsaasdttweadasdsaasdttweadasdsaasdttweadasdsaasd
+ttwetwettweadasdsaasdttweadasdsaasdttweadasdsaasdttweadasdsaasdttweadasdsaasd
+ttweadasdsaasdttweadasdsaasdttweadasdsaasdttweadasdsaasdttwetwe
+ttweadasdsaasd
+ttwe
+t
+wet
+wet
+teat et wet w
+wet
+wet
+teat et wet w
+wet
+wet
+teat et wet w
+ttwe
+t
+wet
+wet
+teat et wet w
+wet
+wet
+teat et wet w
+wet
+wet
+teat et wet w
+t
+wet
+wet
+teat et wet wvv
+wet
+wet
+teat et wet w
+wet
+wet
+teat et wet wtwe
+t
+wet
+wet
+teat et wet w
+ttwe
+t
+wet
+wet
+teat et wet w
+wet
+wet
+teat et wet w
+wet
+wet
+teat et wet wv
+wet
+wet
+teat et wet wwe
+t
+wet
+wet
+teat et wet w
+t
+wet
+wet
+teat et wet wtwe
+t
+wet
+wet
+teat et wet w
+wet
+wet
+teat et wet w
+t
+wet
+wet
+teat et wet wtwe
+t
+wet
+wet
+teat et wet w
+etrtwe
+t
+wet
+wet
+teat et wet w
+etrewwwwwwwwwerwerw
+wer
+ewr
+wer
+
+rwe
+erw
+ew
+ewr
+ewr
+wer
+twe
+t
+wet
+wet
+teat et wet wtwe
+t
+wet
+wettwe
+t
+wet
+wet
+teat et wet wtwe
+t
+wet
+wet
+teat et wet wtwe
+t
+wet
+wet
+teat et wet w
+teat et wet w
