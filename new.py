1dc6f9a34457e6babca5e4e97478ed309e852288
Mikhail <Mikhail.Kapusta@jetbrains.com>
2015-07-16 19:16:36 +0300

    xxx #UIT-2 Fixed Critical Exception

--- new.py
+++ new.py
@@ -9,5 +9,7 @@ qqqqqqqqqq456
 576657
 576\
 
-sdassad
+sdassadqweqweqwe
+qwqwewq
+eqwe
 "C:\Program Files\Java\jdk1.7.0_75\jre\bin\java" -Xmx2048m -XX:MaxPermSize=128M -Duser.home=c:\YouTrack\release6.5\15584 -Djetbrains.youtrack.baseUrl="http://unit-276:8899/youtrack" -Djetbrains.youtrack.disableMailboxIntegration=true -Djetbrains.youtrack.disableGithubIntegration=true -Djetbrains.youtrack.licenseName=K -Djetbrains.youtrack.licenseKey=ce615e719f567d1b575f2afe88b2723456b06d58a65c11bdbd5567eb79aefcc145d6da30eb3db0ee1212c55d3d4c77af426729306355e253caf2a71431c3f994aafca78c4da9a3a4502a35b5ba63741113ea4c0f402da9f2603947305b52ed5bfe28028af82fc1acf7f40217eb112c7c8d5f577d28aef1177ea62e54a0fd3870 -jar youtrack-15584.jar 8899/youtrack
